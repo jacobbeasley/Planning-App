4ac9f079ded76e30269f7f7d7b273e8b63e19a5f
Jacob Beasley <jacob.beasley@fieldnation.com>
2014-09-03 23:38:48 -0500

    Worked more on the basic framework of the mobile app

--- html/js/app.js
+++ html/js/app.js
@@ -1,37 +1,69 @@
+// 
 // init app
 var tripperApp = angular.module('tripperApp', ["ui.router", 'ionic']);
 
+// 
 // global (shared) service
 
+//
 // setup routing
 tripperApp.config(function($stateProvider, $urlRouterProvider) {
-  //
   // For any unmatched url, redirect to /state1
   $urlRouterProvider.otherwise("/splash");
 
-  //
   // Now set up the states
   $stateProvider
     .state('splash', {
       url: "/splash",
       templateUrl: "partials/splash.html",
       controller: "splashCtrl"
+    })
+    .state("login", {
+    	url: "/login",
+    	templateUrl: "partials/login.html",
+    	controller: "loginCtrl"
+    })
+    .state("signup", {
+    	url: "/signup",
+    	templateUrl: "partials/signup.html",
+    	controller: "signupCtrl"
     });
 });
 
+//
 // setup app
-tripperApp.setupApp = function($rootScope) {
+tripperApp.setupApp = function($rootScope, $ionicSideMenuDelegate) {
 	$rootScope.session = 	{
 		user: {}
 	};
 
 	// @TODO - load session data if already logged in
-
+	$rootScope.toggleLeftMenu = function() {
+		$ionicSideMenuDelegate.toggleLeft();
+	}
 };
 
+//
 // splash controller
-var splashCtrl = tripperApp.controller('splashCtrl', function($scope, $rootScope, $ionicSideMenuDelegate){
-	tripperApp.setupApp($rootScope);
-	$scope.$ionicSideMenuDelegate = $ionicSideMenuDelegate;
+tripperApp.controller('splashCtrl', function($scope, $rootScope, $ionicSideMenuDelegate){
+	tripperApp.setupApp($rootScope, $ionicSideMenuDelegate);
 	
+});
+
+// 
+// signup controller
+tripperApp.controller("signupCtrl", function() {
+
+});
+
+// 
+// login controller
+tripperApp.controller("loginCtrl", function() {
+
+});
+
+// 
+// menu controller
+tripperApp.controller("menuCtrl", function($scope, $rootScope) {
+
 });
