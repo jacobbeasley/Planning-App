0efba7c4a6846e64fb97702c30dcca2dd391b270
Jacob Beasley <jacob.beasley@fieldnation.com>
2014-12-01 20:44:15 -0600

    A couple tweaks.

--- app.js
+++ app.js
@@ -27,6 +27,38 @@ if (conf.api) {
 
 // will serve http content (if enabled for this server)
 if (conf.http) {
+	// basic auth
+	app.use(function(req, res, next) {
+		var auth;
+
+	    // check whether an autorization header was send    
+	    if (req.headers.authorization) {
+	      // only accepting basic auth, so:
+	      // * cut the starting "Basic " from the header
+	      // * decode the base64 encoded username:password
+	      // * split the string at the colon
+	      // -> should result in an array
+	      auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+	    }
+
+	    // checks if:
+	    // * auth array exists 
+	    // * first value matches the expected user 
+	    // * second value the expected password
+	    if (!auth || auth[0] !== 'trip' || auth[1] !== 'ping') {
+	        // any of the tests failed
+	        // send an Basic Auth request (HTTP Code: 401 Unauthorized)
+	        res.statusCode = 401;
+	        // MyRealmName can be changed to anything, will be prompted to the user
+	        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+	        // this will displayed in the browser when authorization is cancelled
+	        res.end('Unauthorized');
+	    } else {
+	        // continue with processing, user was authenticated
+	        next();
+	    }
+	});
+
 	// forward to HTML if they go to root
 	app.get("/", function(req, res) {
 		res.redirect("/html/");
